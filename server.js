870140b8b98d8fc99a6adb19272b78334501b595
alexislendechy <135558007+alexislendechy@users.noreply.github.com>
2023-09-10 19:18:14 -0600

    server building

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const express = require('express');
+const fs = require("fs");
+const path = require('path');
+const api = require('./db/db.json');
 
-app.use('/api', apiRoutes); // Prefix all API routes with '/api'
-app.use('/', htmlRoutes);   // HTML routes do not need a prefix
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.route("/api/routes")
+    .get((req, res) => res.json(api))
+    .post((req, res) => {
+        
+    })
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
